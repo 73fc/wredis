a3789661bdc7c47bb9bbf00784b7dce558cba2a3
73fc <542591851@qq.com>
2024-06-29 19:13:22 +0800

    feature: support redis-server network layer

--- src/cmd/mod.rs
+++ src/cmd/mod.rs
@@ -27,12 +27,14 @@ pub trait CommandExecutor {
 }
 
 #[enum_dispatch(CommandExecutor)]
+#[derive(Debug)]
 pub enum Command {
     Get(Get),
     Set(Set),
     HGet(HGet),
     HSet(HSet),
     HGetAll(HGetAll),
+    Unrecognized(Unrecognized),
 }
 
 #[derive(Debug)]
@@ -63,6 +65,20 @@ pub struct HSet {
 pub struct HGetAll {
     key: String,
 }
+#[derive(Debug)]
+pub struct Unrecognized;
+
+impl TryFrom<RespFrame> for Command {
+    type Error = CommandError;
+    fn try_from(value: RespFrame) -> Result<Self, Self::Error> {
+        match value {
+            RespFrame::Array(array) => array.try_into(),
+            _ => Err(CommandError::InvalidCommand(
+                "Command must be an Array".to_string(),
+            )),
+        }
+    }
+}
 
 impl TryFrom<RespArray> for Command {
     type Error = CommandError;
@@ -74,15 +90,18 @@ impl TryFrom<RespArray> for Command {
                 b"hget" => Ok(HGet::try_from(v)?.into()),
                 b"hset" => Ok(HSet::try_from(v)?.into()),
                 b"hgetall" => Ok(HGetAll::try_from(v)?.into()),
-                _ => Err(CommandError::InvalidCommand(format!(
-                    "Invalid command: {}",
-                    String::from_utf8_lossy(cmd.as_ref())
-                ))),
+                _ => Ok(Unrecognized.into()),
             },
             _ => Err(CommandError::InvalidCommand(
                 "Command must have a BulkString as the first argument".to_string(),
             )),
         }
+    }
+}
+
+impl CommandExecutor for Unrecognized {
+    fn execute(self, _: &Backend) -> RespFrame {
+        RESP_OK.clone()
     }
 }
 
--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,6 @@ mod backend;
 mod resp;
 
 pub mod cmd;
-
+pub mod network;
 pub use backend::*;
 pub use resp::*;
